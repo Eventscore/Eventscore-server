329621b128c970d5c3f5f21babd0d8255437a240
johntduong <johntduong@yahoo.com>
2017-04-10 10:50:20 -0700

    Fixed test

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ var app = express();
 // events related endpoint
 app.route('/api/events')
   .get(function(req, res) {
-    res.statusCode(200).send('Get all events');
+    res.send('Get all events');
   })
   .put(function(req, res) {
     res.send('Update an event');
   })
   .post(function(req, res) {
-    res.statusCode(201).send('Add an event');
+    res.send('Add an event');
   });
 
 var port = process.env.PORT || 1337;
