17176f6d5b67d0069bce7103240608382edb7327
johntduong <johntduong@yahoo.com>
2017-04-11 10:54:03 -0700

    Request handler with api call now working.

--- server/Events/Events.js
+++ server/Events/Events.js
@@ -1,16 +1,27 @@
-var Artists = require('../../db/Artists/Artists.js');
-var Events = require('../../db/Events/Events.js');
-var SeatGeek = require('seatgeek-js');
-var seatgeek = new SeatGeek(process.env.SEATGEEK_CLIENTID);
-console.log('SeatGeek', SeatGeek, seatgeek);
+var fetch = require('node-fetch');
 
+fetch.Promise = require('bluebird'); 
+
+var Artists = require('../../db/Artists/Artists.js');
+
+var Events = require('../../db/Events/Events.js');
+
+
+//fetch nearby events from db first
+//if events < 10, fetch from api as well
+//sort results by distance
 exports.getNearbyEvents = function(req, res) {
-  return seatgeek
-  .allEvents()
-  .get()
-  .then(function(data) {
-    res.send(data);
-  });
+  console.log('REQ PARAMS', req.params);
+  var latitude = req.params.lat;
+  var longitude = req.params.lon;
+  fetch('https://api.seatgeek.com/2/events?lat=' + latitude + '&lon=' + longitude + '&range=5mi&client_id=' + process.env.SEATGEEK_CLIENTID)
+  .then(function(response) {
+        return response.json();
+    })
+  .then(function(json) {
+        console.log(json);
+        res.send(json);
+    });
 };
 
 exports.updateEvent = function(req, res) {
