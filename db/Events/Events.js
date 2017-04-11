84e3fd5394df619a8cd98e8220a61446b08c1611
johntduong <johntduong@yahoo.com>
2017-04-10 18:53:01 -0700

    Added additional routes for users and events, added request handler files, wrote request handler for getting all events from seatgeek api, refactored code in server/index.js.

--- db/Events/Events.js
+++ db/Events/Events.js
@@ -2,14 +2,13 @@ var mongoose = require('mongoose');
 var db = require('../config.js');
 
 var eventsSchema = mongoose.Schema({
-	name: String,
-	start: Date,
-	created: Date,
-	updated: Date,
-	artists: [{ type: Schema.Types.ObjectId, ref: 'Artists' }],
-	score: Number,
-	venue: { type: Schema.Types.ObjectId, ref: 'Venues' },
-  location: Object
+  name: String,
+  start: Date,
+  created: Date,
+  updated: Date,
+  artists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Artists' }],
+  score: Number,
+  venue: Object, //geolocation will be inside this object
 })
 
 var Events = mongoose.model('Events', eventsSchema);
--- db/EventsRaw/EventsRaw.js
+++ db/EventsRaw/EventsRaw.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var db = require('../config.js');
 
 var eventsrawSchema = mongoose.Schema({
-  eventId: { type: Schema.Types.ObjectId, ref: 'Events' },
+  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Events' },
   data: Object,
 });
 
