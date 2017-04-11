748d83acfdd24fe2c201588d0c759decfbd13f32
johntduong <johntduong@yahoo.com>
2017-04-10 19:34:12 -0700

    Added console.log for testing

--- server/Events/Events.js
+++ server/Events/Events.js
@@ -2,6 +2,7 @@ var Artists = require('../../db/Artists/Artists.js');
 var Events = require('../../db/Events/Events.js');
 var SeatGeek = require('seatgeek-js');
 var seatgeek = new SeatGeek(process.env.SEATGEEK_CLIENTID);
+console.log('SeatGeek', SeatGeek, seatgeek);
 
 exports.getNearbyEvents = function(req, res) {
   return seatgeek
