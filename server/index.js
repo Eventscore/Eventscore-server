81d6d6c3d34fe3d568fabd843a001b419feb0853
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-04-10 19:03:11 -0700

    (feat) Add body parser and make some changes to request handlers

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
 var express = require('express');
 var app = express();
 var Users = require('./Users/Users.js');
@@ -7,8 +10,13 @@ var Events = require('./Events/Events.js');
 //http://localhost:1337/api/events/longitude/-122.406417/latitude/37.785834
 app.get('/api/events/longitude/:lon/latitude/:lat', Events.getNearbyEvents);
 
+// events related endpoint
 app.route('/api/events')
   .put(Events.updateEvent)
+  .post(jsonParser, function(req, res) {
+    console.log('req.body: ', typeof req.body, req.body);
+    res.send('Add an event');
+  });
 
 app.route('/api/users')
   .get(Users.getAllUsers)
