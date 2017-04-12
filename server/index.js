f22b9b916d65a0dc125f2bb17e5cea21566387e7
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-04-11 19:20:14 -0700

    (feat) Added dummy data to server for testing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+var eventsExampleData = require('../db/eventsExampleData');
 
 var express = require('express');
 var app = express();
 var Users = require('./Users/Users.js');
 var Events = require('./Events/Events.js');
+
+// example data
+app.get('/api/events/exampledata', (req, res) => {
+  res.send(eventsExampleData);
+});
 
 //http://localhost:1337/api/events/longitude/-122.406417/latitude/37.785834
 app.get('/api/events/longitude/:lon/latitude/:lat', Events.getNearbyEvents);
