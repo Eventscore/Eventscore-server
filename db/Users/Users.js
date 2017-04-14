2e2ef14a0fecf96f9f29c710eed660b752cec95c
jason kuo <iamjasonkuo@gmail.com>
2017-04-14 12:28:56 -0700

    included auth-logic

--- db/Users/Users.js
+++ db/Users/Users.js
@@ -1,12 +1,35 @@
 var db = require('../config.js');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 
 var usersSchema = mongoose.Schema({
   favevents: [{type: mongoose.Schema.Types.ObjectId, ref: 'Events'}],
   name: String,
   username: String,
   email: String,
+  password: String  
+});
+
+usersSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
 });
 
 var Users = mongoose.model('Users', usersSchema);
+
+Users.comparePassword = function(attemptedPassword, recordedPassword, callback) {
+  bcrypt.compare(attemptedPassword, recordedPassword, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    } else {
+      callback(null, isMatch);
+    }
+  });
+};
+
 module.exports = Users;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
+'user strict';
+
 require('dotenv').config();
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var handler = require('./request-handler');
+
 var app = express();
 var Users = require('./Users/Users.js');
 var Events = require('./Events/Events.js');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cookieParser('We3Zer$coR3'));
+app.use(session({
+  secret: 'We3Zer$coR3',
+  resave: false,
+  saveUninitialized: true
+}));
 
 
 //http://localhost:1337/api/events/longitude/-122.406417/latitude/37.785834
@@ -19,6 +35,9 @@ app.route('/api/users')
 app.route('/api/users/:userid')
   .get(Users.getUser)
   .put(Users.updateUser)
+
+app.post('/auth/users/login', handler.userLogin);
+app.post('/auth/users/signup', handler.userSignUp);  
 
 var port = process.env.PORT || 1337;
 app.listen(port);
