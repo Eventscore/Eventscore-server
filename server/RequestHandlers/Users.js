3d9d625d175ff46f11176c5c23e8e7710c82a92f
Edwin Brower <edwin.brower@gmail.com>
2017-04-22 12:25:52 -0700

    Correct syntax in user handler

--- server/RequestHandlers/Users.js
+++ server/RequestHandlers/Users.js
@@ -9,16 +9,16 @@ exports.userLogin = function(req, res) {
   console.log('username: ', username);
   console.log('password: ', password);
 
-  Users.findOne({ "username": username })
+  Users.findOne({ 'username': username })
     .exec(function(err, user) {
-      if(!user) {
+      if (!user) {
         console.log('user does not exist bro?');
-        res.status(500).send("username or password doesn't match with our record");
+        res.status(500).send('username or password doesn\'t match with our record');
       } else {
         Users.comparePassword(password, user.password, function(err, match) {
-          if(err) {
+          if (err) {
             console.log('password does not match bro?');
-            res.status(500).send("username or password doesn't match with our record");
+            res.status(500).send('username or password doesn\'t match with our record');
           } else {
             req.session.regenerate(function() {
               req.session.user = user;
@@ -36,10 +36,10 @@ exports.userSignUp = function(req, res) {
   var password = req.body.password;
   var email = req.body.email;
 
-  Users.findOne({ "username": username})
+  Users.findOne({ 'username': username})
     .exec(function(err, user) {
-      if(!user) {
-        Users.create({"name": name, "username": username, "password": password, "email": email});
+      if (!user) {
+        Users.create({'name': name, 'username': username, 'password': password, 'email': email});
         res.json('Account created');
       } else {
         res.json('Username is already taken');
